b94d955b305bebc613412008cd9ea5e1a38c5780
juani <juana100295@gmail.com>
2024-08-05 20:28:23 -0500

    Hola mundo

---
+++ src/SumaBasica.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class SumaBasica {
+    public static void main(String[] args) {
+
+        // solicitar dos números y sumarlos
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int b = scanner.nextInt();
+
+        int suma = a + b;
+        System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+    }
+}
