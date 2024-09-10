d4313c8cfa4057e98194e154d5d5eebc55d13feb
juani <juana100295@gmail.com>
2024-09-09 20:12:01 -0500

    Aprendizaje de Operadores

--- src/PasarPorReferencia2.java
+++ src/PasarPorReferencia2.java
@@ -3,19 +3,20 @@
         private String nombre;
 
         public void modificarNombre(String nuevoNombre){
+
             this.nombre = nuevoNombre;
         }
 
         public String leerNombre(){
             return this.nombre;
         }
-
-
     }
+
+
     public class PasarPorReferencia2 {
         public static void main(String[] args) {
             Persona persona= new Persona();
-            persona.modificarNombre("Juan");
+            persona.modificarNombre("Juanita");
             System.out.println("Iniciamos con el metodo main");
             System.out.println("persona.nombre = " + persona.leerNombre());
 
@@ -32,7 +33,6 @@
         public static void test(Persona persona) {
             System.out.println("Iniciamos el metodo test con ");
            persona.modificarNombre("Pedro");
-
             System.out.println("Finaliza el metodo test con");
 
         }
